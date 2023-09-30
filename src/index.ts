e6847074ccfe1d7606b3eafda22c3632a011a12a
EllAchE <26192612+EllAchE@users.noreply.github.com>
2023-09-29 21:35:00 -0700

    add chess js minimal into folder for ease of use

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { gameChunks } from './fileReader';
-import { initializeMetricMaps } from './prepare';
+import { getGameHistory, initializeMetricMaps } from './prepare';
 
 async function main() {
   const year = 2013;
@@ -11,16 +11,16 @@ async function main() {
   let count = 0;
   for await (const game of games) {
     count++;
-    console.log(count);
-    // const history = getGameHistory(game.moves);
-    // for (const move of history) {
-    //   if (move.captured) {
-    //     board[move.to].deaths[move.captured]++;
-    //     board[move.from].kills[move.captured]++;
-    //   }
-    // }
+    const history = getGameHistory(game.moves);
+    for (const move of history) {
+      if (move.captured) {
+        board[move.to].deaths[move.captured]++;
+        board[move.from].kills[move.captured]++;
+      }
+    }
   }
 
+  require('fs').writeFileSync('test.json', JSON.stringify(board, null, 2));
   console.log(board);
 }
 
