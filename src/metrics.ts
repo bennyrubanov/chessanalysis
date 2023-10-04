a5c34a80cafe36c6468a6a684f50117f00165cf0
EllAchE <26192612+EllAchE@users.noreply.github.com>
2023-10-04 01:59:08 -0700

    add stubs, expect to hook most into the loadPGN

--- src/metrics.ts
+++ src/metrics.ts
@@ -27,13 +27,16 @@ export function getMoveDistance(start: string, end: string): number {
 // function checkForCapture(board: Chess, move: string) {}
 
 // start from back of history. For this to be accurate we need to know which piece checks the king at this index
-function getMateAssists(history: GameHistory[]) {
-  const wasMate = history[history.length - 1].originalString.includes('#');
-  if (wasMate) {
-    const matingPiece 
-    return history[history.length - 2].color;
-  }
-}
+// the edge case here is when pieces "share" a mate, or check. This can be at most 2 due to discovery checks
+// the board configuration will also have to be as it was in the instance of checkmate, or the previous mate.
+// So this is going to need to hook into the loadPGN, probably.
+// function getMateAssists(history: GameHistory[], chess: Chess) {
+//   const wasMate = history[history.length - 1].originalString.includes('#');
+//   if (wasMate) {
+//     const matingPieceSquares = chess._attackFromSquare();
+//     return history[history.length - 2].color;
+//   }
+// }
 
 // This one could get complex if lib doesn't work https://github.com/jhlywa/chess.js/blob/master/README.md#isgameover
 function determineEndType() {}
@@ -48,6 +51,6 @@ function miscChecksFromMove() {
   // check
 }
 
-function isFork() {
-  // a
-}
+// function isFork(move, moveIndex, chess: Chess) {
+//   if
+// }
