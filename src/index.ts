a2abc1e8fb0f659b1ebf8d4b87f224a25955afb2
EllAchE <26192612+EllAchE@users.noreply.github.com>
2023-10-09 00:36:07 -0700

    hk

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,29 @@ import { Chess } from '../cjsmin/src/chess';
 import { gameChunks } from './fileReader';
 import { getGameHistory, initializeMetricMaps } from './prepare';
 
+// const year = 2013;
+// const month = '01';
+// const path = `data/lichess_db_standard_rated_${year}-${month}_TEST_SET.pgn`;
+
+/**
+ * We currently only support analyzing a single game at a time
+ *
+ * @param path
+ * @returns
+ */
 export async function main(path: string) {
-  const year = 2013;
-  const month = '01';
-  // const path = `data/lichess_db_standard_rated_${year}-${month}_TEST_SET.pgn`;
-  // const path = `data/short.pgn`;
   const metrics = initializeMetricMaps();
   const games = gameChunks(path);
   const board = new Chess();
 
   let count = 0;
-  let histories = [];
   for await (const game of games) {
+    // progress tracker
     count++;
-    // console.log(count);
+    if (count % 200 == 0) {
+      console.log(count);
+    }
+
     const history = getGameHistory(board, game.moves);
     for (const move of history) {
       if (move.captured) {
@@ -23,35 +32,8 @@ export async function main(path: string) {
         metrics[move.from].kills[move.captured]++;
       }
     }
-    histories.push(history);
   }
-
-  const histShort = require('fs')
-    .readFileSync('historiesShort.json')
-    .toString()
-    .trim();
-  if (histShort == JSON.stringify(histories, null, 2).trim()) {
-    console.log('histories match');
-  } else {
-    console.log('histories do not match');
-    require('fs').writeFileSync(
-      'badhistory.json',
-      JSON.stringify(histories, null, 2)
-    );
-  }
-
-  return {
-    histShort,
-    historiesString: JSON.stringify(histories, null, 2),
-  };
 }
-
-// import { Chess } from 'chess.js';
-
-// const b = new Chess();
-// b.reset();
-// console.log(b._board);
-// require('fs').writeFileSync('test.json', JSON.stringify(b._board, null, 2));
 
 if (require.main === module) {
   main(`data/short.pgn`).then((res) => {});
--- tests/shortPgn.test.ts
+++ tests/shortPgn.test.ts
@@ -2,7 +2,14 @@ import { main } from '../src';
 
 describe('Using modified cjsmin returns expected results', () => {
   it('should not throw an error', async () => {
-    const { histShort, historiesString } = await main(`data/short.pgn`);
+    const histShort = require('fs')
+      .readFileSync('historiesShort.json')
+      .toString()
+      .trim();
+
+    const histories = await main(`data/short.pgn`);
+    const historiesString = JSON.stringify(histories, null, 2);
+
     expect(histShort.trim()).toEqual(historiesString.trim());
   });
 
