459a6d6dce611b96daf71f1e78cc6c429d7386d6
Benny Rubanov <106097466+bennyrubanov@users.noreply.github.com>
2023-10-13 15:37:44 +0800

    getMoveDistanceSetOfGames function in metrics.ts + unfinished getKillDeathRatios function (#13)
    * new test set and scratch.ts to count games
    
    * new getAverageDistance function + changes to index.ts to calc pieceThatMovedTheFurthest for a set of games
    
    * moved a bunch of code from index.ts into getMoveDistanceSetOfGames function in metrics.ts, new unfinished function for kill/death ratio

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,23 @@
+import {
+  Chess,
+  Color,
+  PieceSymbol,
+  Square,
+  UnambiguousPieceSymbols,
+} from '../cjsmin/src/chess';
+
 export interface FileReaderGame {
   moves: string;
   metadata: string[];
 }
+
+export type GameHistoryMove = {
+  originalString: string;
+  color: Color;
+  from: Square;
+  to: Square;
+  piece: PieceSymbol;
+  captured?: PieceSymbol;
+  promotion?: PieceSymbol;
+  flags: string;
+};
