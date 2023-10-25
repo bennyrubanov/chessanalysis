fe8357fe566ec12b9409d62381a9ea59c424898e
EllAchE <26192612+EllAchE@users.noreply.github.com>
2023-10-25 00:03:59 -0700

    use a valid pgn

--- tests/historyGenerator.test.ts
+++ tests/historyGenerator.test.ts
@@ -8,13 +8,19 @@ describe('Chess', () => {
     it('should generate the correct move history for a given PGN string', () => {
       const chess = new Chess();
       const moveString =
-        '1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. Ba4 Nf6 5. O-O Be7 6. Re1 b5 7. Bb3 d6 8. c3 O-O 9. h3 Na5 10. Bc2 c5 11. d4 Qc7 12. Nbd2 Bd7 13. Nf1 Rfc8 14. d5 Nc4 15. b3 Nb6 16. Ng3 g6 17. Bh6 c4 18. b4 a5 19. a3 Ra6 20. Qd2 Rca8 21. Rac1 axb4 22. axb4 Ra2 23. Nh2 R8a3 24. f4 Na4 25. fxe5 dxe5 26. Bxa4 Rxd2 27. Bxd2 Rxa4 28. Ra1 Qa7+ 29. Be3 Rxa1 30. Bxa7 Rxe1+ 31. Kf2 Rc1 32. Ne2 Rc2 33. Ke3 Nxe4 34. Kxe4 Rxe2+ 35. Kf3 Rc2 36. Bc5 Rxc3+ 37. Ke4 Bf6 38. Ng4 Bf5#';
+        '1. e4 e6 2. d4 b6 3. a3 Bb7 4. Nc3 Nh6 5. Bxh6 gxh6 6. Be2 Qg5 7. Bg4 h5 8. Nf3 Qg6 9. Nh4 Qg5 10. Bxh5 Qxh4 11. Qf3 Kd8 12. Qxf7 Nc6 13. Qe8# 1-0';
 
       const gen = chess.historyGenerator(moveString);
       const expectedMoves = moveString.split(' ').filter((_, i) => i % 3 !== 0);
+      expectedMoves.pop(); // remove the result from the end of the array
       for (const { move, board } of gen) {
+        // console.log(board);
+        console.log(move);
         // validate that the piece that was moved is of the same type as the destination square
         expect(move.piece).toEqual(board[move.toIndex].type);
+        expect(move.unambiguousSymbol).toEqual(
+          board[move.toIndex].unambiguousSymbol
+        );
         // validate that the original string in the move matches the expected string
         expect(move.originalString).toEqual(expectedMoves.shift());
       }
