91610a61acc2d8800510cf75df6325b7fa0f936c
Ell AchE <26192612+EllAchE@users.noreply.github.com>
2023-10-24 22:53:14 -0700

    Support UAH in moves as well as history (#25)
    * ic
    
    * fix unambig in capture
    
    * add uah to moves also
    
    * update test data to include uas
    
    * Update chess.ts
    
    adding comments for inferPieceType
    
    ---------
    
    Co-authored-by: Benny Rubanov <106097466+bennyrubanov@users.noreply.github.com>

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,8 @@
-import { Color, PieceType, Square } from '../cjsmin/src/chess';
+import { PieceType, Square } from '../cjsmin/src/chess';
 
 export interface FileReaderGame {
   moves: string;
   metadata: string[];
 }
 
-export interface GameHistoryMove {
-  originalString: string;
-  color: Color;
-  from: Square;
-  to: Square;
-  piece: PieceType;
-  captured?: PieceType;
-  promotion?: PieceType;
-  flags: string;
-}
 
-export interface Capture {
-  captured: PieceType;
-  capturedBy: PieceType;
-  captureSquare: Square;
-  originSquare: Square;
-}
