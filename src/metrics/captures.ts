f0809aec0413a17fd5af8b7e060eeee16418c82f
EllAchE <26192612+EllAchE@users.noreply.github.com>
2023-10-27 02:57:48 -0700

    add streaks and revenge kills

--- src/metrics/captures.ts
+++ src/metrics/captures.ts
@@ -1,42 +1,112 @@
-// /**
-//  * Track where captures occur on the board, what piece was captured, by what piece and where the capturing piece was
-//  */
+import {
+  ALL_SQUARES,
+  ALL_UNAMBIGUOUS_PIECE_SYMBOLS,
+  PrettyMove,
+  UnambiguousPieceSymbol,
+} from '../../cjsmin/src/chess';
 
-// function initializeCaptureHistory() {
-//   const captureHistory = {};
+export function createBoardMap(): BoardMap {
+  /**
+   * i.e.
+   * a1: {
+   *   k: {
+   *    captured: 0,
+   *    captures: 0,
+   *   },
+   * },
+   */
+  const squareCaptureInfo: {
+    [key: string]: {
+      [key: string]: {
+        captured: number;
+        captures: number;
+        revengeKills: number;
+      };
+    };
+  } = {};
 
-//   // The map value corresponds to the number of times this piece TOOK on the square, not was captured
-//   const squareHistory: Record<string, number> = {
-//     p: 0,
-//     k: 0,
-//     q: 0,
-//     r: 0,
-//     b: 0,
-//     n: 0,
-//     P: 0,
-//     K: 0,
-//     Q: 0,
-//     R: 0,
-//     B: 0,
-//     N: 0,
-//   };
+  for (const square of ALL_SQUARES) {
+    squareCaptureInfo[square] = {};
+    for (const piece of ALL_UNAMBIGUOUS_PIECE_SYMBOLS) {
+      squareCaptureInfo[square][piece] = {
+        captured: 0,
+        captures: 0,
+        revengeKills: 0,
+      };
+    }
+  }
 
-//   for (const square of BOARD_SQUARES) {
-//     // The map value corresponds to the number of times this piece TOOK on the square, not was captured
-//     const squareTakeHistory: Record<string, number> = { ...squareHistory };
+  return squareCaptureInfo;
+}
 
-//     // This map is for the number of times a piece was captured on a square
-//     const squareCaptureHistory: Record<string, number> = { ...squareHistory };
-//     captureHistory[square] = {};
-//   }
+interface BoardMap {
+  [key: string]: {
+    [key: string]: {
+      captured: number;
+      captures: number;
+      revengeKills: number;
+    };
+  };
+}
 
-//   return captureHistory;
-// }
+export function trackCaptures(boardMap: BoardMap, moves: PrettyMove[]) {
+  let lastMove: PrettyMove;
+  let i = 0;
+  for (const move of moves) {
+    if (move.capture) {
+      boardMap[move.to][move.unambiguousSymbol].captures++;
+      boardMap[move.to][move.capture.unambiguousSymbol].captured++;
+      // revenge kills
+      if (lastMove.capture && move.to === lastMove.to) {
+        boardMap[move.to][move.unambiguousSymbol].revengeKills++;
+      }
+    }
+    lastMove = move;
+    i++;
+  }
+}
 
-// // currently we are only doing this by piece type (and color). We could add disambiguation
-// function trackCaptures(game: GameHistoryMove[]) {
-//   for (const move of game) {
-//     if (move.capture) {
-//     }
-//   }
-// }
+function uapMap() {
+  const map = {};
+  for (const uap of ALL_UNAMBIGUOUS_PIECE_SYMBOLS) {
+    map[uap] = { killStreaks: 0 };
+  }
+  return map;
+}
+
+function getMaxKillStreak(
+  uapMap: any,
+  moves: PrettyMove[],
+  startingIndex: 0 | 1 // assume games have at least 2 moves
+) {
+  let i = startingIndex;
+  let streakLength = 0;
+  let streakPiece: UnambiguousPieceSymbol;
+
+  while (i < moves.length) {
+    const move = moves[i];
+    if (move.capture) {
+      if (streakLength === 0) {
+        streakPiece = move.unambiguousSymbol;
+        streakLength++;
+      } else if (streakPiece === move.unambiguousSymbol) {
+        streakLength++;
+      } else {
+        uapMap[streakPiece].killStreaks = Math.max(
+          uapMap[streakPiece].killStreaks,
+          streakLength
+        );
+        streakLength = 0;
+      }
+    }
+    i += 2;
+  }
+}
+
+export function getBWKillStreaks(moves: PrettyMove[]) {
+  const tracker = uapMap();
+  getMaxKillStreak(tracker, moves, 0);
+  getMaxKillStreak(tracker, moves, 1);
+
+  return tracker;
+}
