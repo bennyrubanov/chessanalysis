a9db93d3d4164f9e3221657c0190ec742fd5325a
Benny Rubanov <106097466+bennyrubanov@users.noreply.github.com>
2023-10-27 16:35:06 +0800

    super quick update

--- src/metrics/metrics.ts
+++ src/metrics/metrics.ts
@@ -194,6 +194,7 @@ export async function getMoveDistanceSetOfGames(games: FileReaderGame[]) {
     pieceThatMovedTheFurthest,
     maxDistance,
     gameCount,
+    gameWithFurthestPiece,
     siteWithFurthestPiece,
     totalDistanceMap,
     lastGame,
